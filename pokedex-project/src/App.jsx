0c9b5dc4432cf9919439113ad6bb82cf14dc4d7d
colecrum26 <cwcrumrine@gmail.com>
2023-04-11 21:36:59 -0500

    set up fetch

--- pokedex-project/src/App.jsx
+++ pokedex-project/src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Pokedex from "./components/pokemon";
 
 function App() {
   return (
--- pokedex-project/src/components/pokemon.jsx
+++ pokedex-project/src/components/pokemon.jsx
@@ -1,2 +1,20 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+function Pokedex(props) {
+    const [list, setList] = useState([]);
+    const [searchPokemon, setSearchPokemon] = useState("");
+
+    function getPokemon() {
+        fetch("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
+        .then((res) => {
+            return res.json();
+        })
+        .then((result) => {
+            setList(result);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+}
